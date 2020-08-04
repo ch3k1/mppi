09b5ed7311cd7354076e97ba3d367bfb4a65eeda
Aleksandre Vardanidze <avardanidze@gmail.com>
2020-08-04 08:33:42 +0400

    Update max_pod_per_instance.sh
    Add the number of worker nodes

--- max_pod_per_instance.sh
+++ max_pod_per_instance.sh
@@ -2,20 +2,22 @@
 
 # N is the number of Elastic Network Interfaces (ENI) of the instance type
 # M is the number of IP addresses of a single ENI
-# K kubeconfig path
+# K is path of kube config
+# I is the number of worker nodes on AWS
 
-while getopts n:m:k: flag
+while getopts n:m:k:i: flag
 do
     case "${flag}" in
         n) interfaces=${OPTARG};;
         m) addresses=${OPTARG};;
         k) kubeconfig=${OPTARG};;
+        i) instances=${OPTARG};;
     esac
 done
 
 if [ -z "$kubeconfig" ]
 then
-   exit
+   kubeconfig=$HOME/.kube/config
 fi
 if [ -z "$interfaces" ] || [ -z "$addresses" ]
 then
@@ -23,8 +25,11 @@ then
 fi
 
 pods=$(kubectl --kubeconfig="$kubeconfig" get pods --field-selector=status.phase=Running --all-namespaces -o json | jq '.items | length')
-
-# N * (M - 1) + 2
-num=$(($interfaces * ($addresses-1) + 2))
-
+if [ -z "$instances" ]
+then
+   num=$(($interfaces * ($addresses-1) + 2))
+else
+   value=$(($interfaces * ($addresses-1) + 2))
+   num=$(($value * $instances))
+fi
 echo $((num - pods))
